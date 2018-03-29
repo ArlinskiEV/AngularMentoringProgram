1de33911deda5d115abac552f633f0fc8110dea9
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-29 18:10:17 +0300

    AMP/task3_observable/features: IT IS ALIVE

--- angular-starter/src/app/angular-intro/core/services/modalWindow.service.ts
+++ angular-starter/src/app/angular-intro/core/services/modalWindow.service.ts
@@ -7,7 +7,7 @@ export class ModalWindowServices {
   // public message = 'Are you shure?';
   // public answerArr = ['Yes', 'No'];
   public data = {
-    visible: false,
+    // visible: false,
     // click: (answer: string) => Promise.resolve(answer),
     message: 'Are you shure?',
     answerArr: ['Yes', 'No'],
@@ -16,29 +16,25 @@ export class ModalWindowServices {
   // public result: Promise<string>;
 
   // protected callback: (message: string) => void;
-  protected source: Observable<string>;
-  private sound: Observable<string>;
+  private source: Observable<string>;
 
   constructor() {
     console.log('### ModalWindowServices constructor ###');
     // create observer
-    this.source = new Observable((observer) => {
-      console.warn('observ in modal-serv');
-      console.log('### ModalWindowServices.observable ###');
-      const listener = this.sound.subscribe(
-        (message) => observer.next(message),
-        (error) => console.error(`error modalWindow.service:${error}`),
-        () => {
-          console.log('### ModalWindowServices.observable:  sound is done###');
-          observer.complete();
-        }
-      );
-      console.warn('observ end in modal-serv');
-    });
+    // this.source = new Observable((observer) => {
+    //   console.warn('observ in modal-serv');
+    //   console.log('### ModalWindowServices.observable ###');
+    //   const listener = this.sound.subscribe(
+    //     (message) => observer.next(message),
+    //     (error) => console.error(`error modalWindow.service:${error}`),
+    //     () => {
+    //       console.log('### ModalWindowServices.observable:  sound is done###');
+    //       observer.complete();
+    //     }
+    //   );
+    //   console.warn('observ end in modal-serv');
+    // });
 
-  }
-  public click(answer: string) {
-    return Promise.resolve(answer);
   }
 
   public show(
@@ -65,12 +61,12 @@ export class ModalWindowServices {
     //     //   }, text: item};
     //     // })
     // };
-    this.data.visible = true; console.warn('show');
+    // this.data.visible = true; console.warn('show');
     this.data.message = message;
     this.data.answerArr = answerArr;
 
     return new Observable((observer) => {
-      console.warn('observ in modal-serv-return');
+      // console.warn('observ in modal-serv-return');
       const listener = this.source.subscribe(
         (x) => { // resend all from source in new Observable
           console.log(`resent:${x}`);
@@ -79,12 +75,14 @@ export class ModalWindowServices {
         (error) => { console.log(`### ModalWindowServices.show.error: ${error} ###`); },
         () => {
           console.log('### ModalWindowServices.done? ###');
-          this.data.visible = false;
+          // this.data.visible = false;
           console.warn('hide');
+          observer.complete();
         }
       );
       // observer.next('---');
       console.warn('observ end in modal-serv-ret');
+      // listener.unsubscribe();
     });
 
     // return new Promise((res) => {
@@ -104,6 +102,6 @@ export class ModalWindowServices {
   // }
 
   public listenMe(source: Observable<string>) {
-    this.sound = source;
+    this.source = source;
   }
 }
--- angular-starter/src/app/angular-intro/modalWindow/modalWindow.component.ts
+++ angular-starter/src/app/angular-intro/modalWindow/modalWindow.component.ts
@@ -12,25 +12,55 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ModalWindowComponent implements OnInit {
   protected source: Observable<string>;
-  protected doIt: () => string;
+  protected doIt: Promise<string>;
+  private data: any;
   constructor(private modalWindowServices: ModalWindowServices) {
     console.log('ModalWindowComponent constructor');
-    this.doIt = () => 'constructor';
+
+    // --------------------------------block
+    const obj = this.modalWindowServices.data.answerArr
+          .map((item) => {
+            return {click: () => item, text: item};
+          });
+
+    this.data = {
+      ...this.modalWindowServices.data,
+      visible: false,
+      answerArr: obj,
+    };
+    // -------------------------------------
   }
 
   public ngOnInit() {
     console.log('ModalWindowComponent ngOnInit');
     this.source = new Observable((observer) => {
       console.warn('observ in modal-comp');
-      console.log('observable in modalWindowComponent');
-      // observer.next('hello from observable modalWindowComponent');
-      console.warn(`modal-window-comp: ${this['doIt']()}`);
+      // console.log('observable in modalWindowComponent');
 
-      // observer.next(this['doIt']());
-      // observer.complete();
-      // console.warn('observ end in modal-comp');
+      // -----------------------------------------------------
+      this.doIt = new Promise((res, rej) => {
+        console.log('getter data');
+        this['bad'] = () => res('Close');
+        const obj = this.modalWindowServices.data.answerArr
+          .map((item) => {
+            return {click: () => res(item), text: item}; /// REALLY?????
+          });
 
-      setTimeout(() => observer.next(this['doIt']()), 10000);
+        this.data = {
+          ...this.modalWindowServices.data,
+          visible: true,
+          answerArr: obj,
+        };
+      });
+      // -----------------------------------------------------
+      this.doIt
+        .then((result) => {
+          observer.next(result);
+        })
+        .then(() => {
+          this.data.visible = false;
+          observer.complete();
+        });
     });
 
     this.modalWindowServices.listenMe(this.source);
@@ -38,14 +68,9 @@ export class ModalWindowComponent implements OnInit {
 
   protected handler(result: string) {
     console.log(`modal: ${result}`);
-    // this.modalWindowServices.answer(result);
-    this.doIt = () => result;
+    this['bad']();
   }
-  protected close() {
-    console.log(`modalWindowComponent: modal was closed without answer`);
-    // this.modalWindowServices.answer('Close');
-    this.doIt = () => 'Close';
-  }
+
   // get visible() {
   //   return this.modalWindowServices.visible;
   // }
@@ -59,17 +84,20 @@ export class ModalWindowComponent implements OnInit {
   //     });
   // }
 
-  get data() {
-    const obj = this.modalWindowServices.data.answerArr
-        .map((item) => {
-          return {click: () => this.handler(item), text: item};
-        });
+//
 
-    const t = 1;
+  // get data() {
+  //   console.log('getter data');
+  //   const obj = this.modalWindowServices.data.answerArr
+  //       .map((item) => {
+  //         return {click: () => this.handler(item), text: item};
+  //       });
 
-    return {
-      ...this.modalWindowServices.data,
-      answerArr: obj,
-    };
-  }
+  //   const t = 1;
+
+  //   return {
+  //     ...this.modalWindowServices.data,
+  //     answerArr: obj,
+  //   };
+  // }
 }
