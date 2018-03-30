91ca5b65f0b2f43b0aa53748bb99504561c9be34
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-30 13:40:10 +0300

    AMP/task3_authorization/features: done

--- angular-starter/src/app/angular-intro/core/services/authorization.service.ts
+++ angular-starter/src/app/angular-intro/core/services/authorization.service.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
+import { User } from '../entities';
 
 @Injectable()
 export class AuthorizationService {
-  private userId = 0;
+  private user: User = {
+    id: 0,
+    userName: 'NoName',
+  };
   constructor() {
     console.log('### AuthorizationService constructor ###');
   }
-  public login() {
+  public login(payload: any) {// how do it right??
     // Login (stores fake user info and token to local storage)
+
+    console.log(`name:${payload.login}, pass:${payload.password}`);
+    // userId = verify(payload);
+    this.user.userName = payload.login;
+    this.user.id = Math.trunc(1 + Math.random() * 10);
   }
   public logout() {
     // Logout (wipes fake user info and token from local storage)
+    this.user = {id: 0, userName: 'NoName', };
   }
   public isAuthenticated() {
     // IsAuthenticated (boolean)
     console.log('### AuthorizationService.isAuthenticated ###');
-    return !!this.userId;
+    return !!this.user.id;
   }
   public getUserInfo() {
     // GetUserInfo (returns user login)
+    return {
+      login: this.user.userName,
+    };
   }
 
 }
--- angular-starter/src/app/angular-intro/login-page/login-page.component.ts
+++ angular-starter/src/app/angular-intro/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthorizationService } from '../core';
 
 @Component({
   selector: 'login-page',
@@ -10,7 +11,16 @@ import { NgForm } from '@angular/forms';
 })
 
 export class LoginPageComponent {
-  constructor() {
+  protected login: string;
+  protected password: string;
+  constructor(private authorizationService: AuthorizationService) {
     console.log('login-page-constructor');
+    this.login = '';
+    this.password = '';
+  }
+
+  public click() {
+    // console.log(`name:${this.login}, pass:${this.password}`);
+    this.authorizationService.login({login: this.login, password: this.password});
   }
 }
