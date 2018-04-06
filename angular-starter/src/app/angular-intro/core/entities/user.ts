0f7a4a6faf448f358de2241acc005ff25e5deff7
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-06 15:39:41 +0300

    AMP/task7/features: server + 1)a: store token

--- angular-starter/src/app/angular-intro/core/entities/user.ts
+++ angular-starter/src/app/angular-intro/core/entities/user.ts
@@ -1,8 +1,20 @@
 export interface User {
   id: number;
   userName: string;
+  token: string;
 }
 
 export interface SharedUserInfo {
   login: string;
+}
+
+export interface Shared {
+  sharedInfo(): SharedUserInfo;
+}
+
+export class MyUser implements Shared {
+  public user: User;
+  public sharedInfo(): SharedUserInfo {
+    return {login: this.user.userName};
+  }
 }
--- angular-starter/src/app/angular-intro/core/services/authorization.service.ts
+++ angular-starter/src/app/angular-intro/core/services/authorization.service.ts
@@ -9,16 +9,29 @@ import { User, SharedUserInfo } from '../entities';
 import { NgZone } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
+import {
+  HttpClient,
+  HttpRequest,
+  HttpEvent,
+  HttpHeaders,
+  HttpParams,
+  HttpResponse
+} from '@angular/common/http';
+
+import { HttpParamsOptions } from '@angular/common/http/src/params';
+
 @Injectable()
 export class AuthorizationService {
   // public mySource: BehaviorSubject<SharedUserInfo>;
   public mySource: ReplaySubject<SharedUserInfo>;
-  private user: User = {
-    id: 0,
-    userName: 'NoName(serv)',
-  };
+  private user: User | null = null;
 
-  constructor(private _ngZone: NgZone) {
+  private baseUrl = 'http://localhost:3004';
+
+  constructor(
+    private _ngZone: NgZone,
+    private http: HttpClient,
+  ) {
     // this.mySource = new BehaviorSubject({login: ''});
     this.mySource = new ReplaySubject(1);
     this.mySource.next({login: ''});
@@ -40,19 +53,57 @@ export class AuthorizationService {
     // ----------------------------------------------------------------
     // verify
     console.log(`name:${payload.login}, pass:${payload.password}`);
-    this.user.id = Math.trunc(1 + Math.random() * 10);
-    this.user.userName = payload.login;
+    this.user = {
+      id: Math.trunc(1 + Math.random() * 10),
+      userName: payload.login,
+      token: 'fakeToken',
+    };
+    // ----------------------------------------------------------------
+    // const path = '/auth/login';
+    // req.body.login (.toUpperCase) ===
+    // req.body.password ===
+
+    const url = `${this.baseUrl}/auth/login`;
+    const method = 'POST';
+    const headers = new HttpHeaders();
+    const options = {
+      fromObject: payload,
+    };
+    const req = new HttpRequest(
+      method,
+      url,
+      payload,
+      {
+        headers,
+        reportProgress: false,
+        // params: new HttpParams(options),
+        params: new HttpParams(),
+        responseType: 'json',
+        withCredentials: false
+      }
+    );
+    // ----------------------------------------------------------------
+    const listener = this.http.request(req)
+      .subscribe(
+        (data: HttpResponse<any>) => {
+          if (data.type) {
+            this.user.token = data.body.token;
+          }
+        },
+        (error) => console.error(`ERROR: ${error.error}`),
+        () => listener.unsubscribe()
+      );
     // ----------------------------------------------------------------
     this.mySource.next({login: this.user.userName});
   }
   public logout(): void {
     // Logout (wipes fake user info and token from local storage)
-    this.user = {id: 0, userName: 'NoName', };
+    this.user = null;
     this.mySource.next({login: ''});
   }
   public isAuthenticated(): boolean {
     // IsAuthenticated (boolean)
-    return !!this.user.id;
+    return !!this.user;
   }
   public getUserInfo(): Observable<SharedUserInfo> {
     return this.mySource.asObservable();
