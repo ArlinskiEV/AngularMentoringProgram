ec88e1ea8f87b680b34e6c82d9b039b54d4dfb5a
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-13 10:45:30 +0300

    AMP/task8/fix: regexp

--- angular-starter/src/app/angular-intro/course-page/date.component.ts
+++ angular-starter/src/app/angular-intro/course-page/date.component.ts
@@ -15,12 +15,21 @@ const CUSTOM_DATE_VALUE_ACCESSOR = {
 
 @Component({
   selector: 'date-component',
+  styles: [`
+    :host>* {
+      border: 5px solid red;
+    }
+  `],
   template: `
     <input type="text"
+      #my
       (change)="setValue($event)"
       placeholder="dd/MM/yyyy"
       [value]="date"
     >
+    <p>date:{{date | json}}</p>
+    <p>value:{{value | json}}</p>
+    <p>input: {{my.value}}</p>
   `,
   // [value]="value | date:'dd/MM/yyyy'
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -45,10 +54,16 @@ export class DateComponent implements ControlValueAccessor {
     }
   }
   public registerOnChange(fn: any): void {
-    this.onChange = fn;
+    this.onChange = (_) => {
+      fn(_);
+      // this._changeDetectorRef.markForCheck();
+    };
   }
   public registerOnTouched(fn: any): void {
-    this.onTouched = fn;
+    this.onTouched = () => {
+      fn();
+      // this._changeDetectorRef.markForCheck();
+    };
   }
 
   // ...it isn't so necessarily as she said
@@ -59,18 +74,22 @@ export class DateComponent implements ControlValueAccessor {
   private onChange = (_) => {};
   private onTouched = () => {};
 
+  // -----------------------------------------------------
+  // -----------------------------------------------------
+
   private set date(newValue: string) {
     console.warn(`set date:${newValue}`);
     if (newValue) {
       this.value = null;
       // check
-      if (newValue.match(/^[?0-2][0-9]\/[?0,1][0-9]\/[1,2][0-9]{3}$/)) {
+      if (newValue.match(/^[0-2]?[0-9]\/[0,1]?[0-9]\/[1,2][0-9]{3}$/)) {
         const t = newValue.split('/');
         this.value = + new Date(`${t[2]}-${t[1]}-${t[0]}`); // YYYY-MM-DD
+        console.warn(`check:true:${this.value}`);
       }
-      this._changeDetectorRef.markForCheck();
       this.onChange(this.value); // string only for view
     }
+    this._changeDetectorRef.markForCheck();
   }
 
   private get date(): string {
