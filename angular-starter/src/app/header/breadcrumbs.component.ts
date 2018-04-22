fb4965c478475a4e6876d8cee12d177eacf0a2b5
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-22 21:58:40 +0300

    AMP/routing/features: breadcrumbs in progress

--- angular-starter/src/app/header/breadcrumbs.component.ts
+++ angular-starter/src/app/header/breadcrumbs.component.ts
@@ -5,7 +5,7 @@ import {
   OnInit,
   OnDestroy,
 } from '@angular/core';
-import { Router, RouterLink, Event, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Router, RouterLink, Event, NavigationEnd, ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { Course, BreadcrumbsService } from '../core';
@@ -18,11 +18,18 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
    a {
      text-decoration: underline;
    }
+   :host > div {
+    display: flex;
+    align-items: center;
+   }
   `],
   template: `
     <div>
       <h3>{{course.name}}</h3>
-      <a (click)="goToCourse()">Courses</a>
+      <a
+        *ngFor=""
+        (click)="goToCourse()"
+      >Courses</a>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -32,7 +39,10 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 export class BreadcrumbsComponent implements OnInit, OnDestroy {
   // public source = new BehaviorSubject(new Course());
   public course: Course = new Course();
-  private listener: Subscription;
+  private params: Params;
+  // ...path from route root?
+  private path;
+  private listeners: Subscription[] = [];
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
@@ -41,15 +51,32 @@ export class BreadcrumbsComponent implements OnInit, OnDestroy {
   ) {}
 
   public ngOnInit() {
-    this.listener = this.breadcrumbsService.getSource()
-      .subscribe(
-        (course: Course) => {
-          // this.source.next(course);
-          this.course = course;
-          this.changeDetectorRef.markForCheck();
-        },
-      )
-    ;
+
+    this.listeners.push(this.activatedRoute.params
+      .subscribe((data: Params) => console.log(`params = ${JSON.stringify(data)}`))
+    );
+
+    this.listeners.push(
+      this.router.events
+        .filter((e: Event) => e instanceof NavigationEnd)
+        .map((_) => {debugger; return this.activatedRoute.params; })
+        .switch()
+        .subscribe(
+          // i don't accept params.... it's work only with right component??
+          (data: Params) => console.log(`params2 = ${JSON.stringify(data)}`)
+        )
+    );
+
+    this.listeners.push(
+      this.breadcrumbsService.getSource()
+        .subscribe(
+          (course: Course) => {
+            // this.source.next(course);
+            this.course = course;
+            this.changeDetectorRef.markForCheck();
+          },
+        )
+    );
   }
 
   public goToCourse() {
@@ -57,6 +84,6 @@ export class BreadcrumbsComponent implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy() {
-    this.listener.unsubscribe();
+    this.listeners.forEach((item) => item.unsubscribe());
   }
 }
