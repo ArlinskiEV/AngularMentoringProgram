a989fac380e5c85a4e42e68abd478a3e39bc5947
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-23 16:52:17 +0300

    AMP/task8/features: date-component

--- angular-starter/src/app/course-page/date.component.ts
+++ angular-starter/src/app/course-page/date.component.ts
@@ -24,13 +24,11 @@ const CUSTOM_DATE_VALUE_ACCESSOR = {
       #my
       (change)="setValue($event.target.value)"
       placeholder="dd/MM/yyyy"
-      [value]="value"
+      [value]="valueToString(value)"
     >
     <p>value:{{value | json}}</p>
     <p>input: {{my.value}}</p>
   `,
-  // (change)="setValue($event)"
-  // [value]="value | date:'dd/MM/yyyy'
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [CUSTOM_DATE_VALUE_ACCESSOR],
 })
@@ -47,13 +45,8 @@ export class DateComponent implements ControlValueAccessor {
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   public writeValue(value: number): void {
-    console.warn(`writeValue:${value}`);
-    if (value !== this.currentValue) {
-      // this.currentValue = value;
-      const result = new Date(value);
-      // this.setValue(value);
-      this.setValue(`${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`);
-    }
+    console.warn('write');
+    this.value = value;
   }
   public registerOnChange(fn: any): void {
     this.onChange = fn;
@@ -62,54 +55,46 @@ export class DateComponent implements ControlValueAccessor {
     this.onTouched = fn;
   }
 
-  // ...it isn't so necessarily as she said
-  // public setDisabledState(isDisabled: boolean): void {
-  //   console.warn('');
-  // }
-
   private onChange = (_) => {};
   private onTouched = () => {};
 
-  // -----------------------------------------------------
-  // -----------------------------------------------------
+  // ------------------------------------------------------------------
+  // set & get date-component value
+  private get value(): number {
+    console.warn('get');
+    return this.currentValue;
+  }
 
-  private set value(newValue: string) {
-    console.warn(`set date:${newValue}`);
-
-    this.currentValue = null;
-    // check
-    if (newValue.match(/^[0-2]?[0-9]\/[0,1]?[0-9]\/[1,2][0-9]{3}$/)) {
-      const t = newValue.split('/');
-      this.currentValue = + new Date(`${t[2]}-${t[1]}-${t[0]}`); // YYYY-MM-DD
-      console.warn(`check:true:${this.currentValue}`);
-    }
-    // this.onChange(this.value); // string only for view
-    this.onChange(this.currentValue);
-
+  // all other methods work with this
+  private set value(newValue: number) {
+    console.warn('set');
+    this.currentValue = newValue;
+    this.onChange(newValue);
     this.changeDetectorRef.markForCheck();
   }
+  // ------------------------------------------------------------------
 
-  private get value(): string {
-    const result = new Date(this.currentValue);
-    // console.warn(`get date:${this.currentValue}`);
-    // console.warn(`result=${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`);
-    return this.currentValue
-      ? `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
-      : ''
-    ;
-  }
-
-  // private setValue(item) {
-  //   console.warn(`setValue:${item.target.value}`);
-  //   this.value = item.target.value;
-  // }
-
+  // ------------------------------------------------------------------
+  // work text-input with number-date
   private setValue(value: string) {
-    console.warn(`setValue:${value}`);
-    this.value = value;
+    console.warn('setValue');
+    let newValue = null; // if format incorrect
+    // check
+    if (value.match(/^[0-2]?[0-9]\/[0,1]?[0-9]\/[1,2][0-9]{3}$/)) {
+      const t = value.split('/');
+      newValue = + new Date(`${t[2]}-${t[1]}-${t[0]}`); // YYYY-MM-DD
+    }
+    this.value = newValue;
   }
+  private valueToString(value: number): string {
+    console.warn('valueToString');
+    const result = new Date(value);
+    return `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`;
+  }
+  // ------------------------------------------------------------------
 }
 // input = text input dd/MM/yyyy format
-// value = real date-value, type = number, + new Date()
-// date = getter/setter string-value, only for view. transform text to number
-// setValue = setter for it
+// currentValue = real date-value, type = number, + new Date()
+// set/get value - work with currentValue + angular
+// setValue: view -> model
+// valueToString: model -> view
