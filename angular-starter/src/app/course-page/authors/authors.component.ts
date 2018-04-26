71856356d17baf3b475fc3237f7ba5d39ddd0b56
ArlinskiEV <ArlinskiEV@gmail.com>
2018-04-26 13:29:54 +0300

    AMP/task8/features: authors fix, done

--- angular-starter/src/app/course-page/authors/authors.component.ts
+++ angular-starter/src/app/course-page/authors/authors.component.ts
@@ -4,9 +4,14 @@ import {
   forwardRef,
   ChangeDetectorRef,
   Input,
+  OnInit,
 } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+
 import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormGroup, FormControl } from '@angular/forms';
 import { Author } from '../../core/entities';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'authors-component',
@@ -18,40 +23,57 @@ import { Author } from '../../core/entities';
       </div>
       <select multiple class="form-control" size="8"
         (blur)="onTouched()"
-        [formControl]="myControl"
+        [(ngModel)]="value"
       >
         <option
-          *ngFor="let author of authorsList"
+          *ngFor="let author of fullAuthorsList"
           [ngValue]="author"
         >{{author.id}} {{author.name.first}} {{author.name.last}}</option>
       </select>
     </div>
-    <p>value:{{value | json}}</p>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [{provide: NG_VALUE_ACCESSOR, useExisting: AuthorsComponent, multi: true}],
 })
 
-export class AuthorsComponent implements ControlValueAccessor {
+export class AuthorsComponent implements ControlValueAccessor, OnInit {
   @Input() public titleString: string = 'myAythors';
-  @Input() public authorsList: Author[] = [];
+  public fullAuthorsList: Author[] = [];
+  @Input() public authorsList: Observable<Author[]>;
 
-  public myControl: FormControl = new FormControl([]);
+  // public myControl: FormControl = new FormControl([]);
+  // [formControl]="myControl"
 
   private currentValue: Author[] = []; // checked Author
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
+  public ngOnInit() {
+    const listener: Subscription = this.authorsList
+      .finally(() => listener.unsubscribe())
+      .subscribe((list) => {
+        this.fullAuthorsList = list;
+        this.currentValue = this.fullAuthorsList
+        .filter((author) => this.currentValue
+          .some((item) => author.id === item.id)
+        )
+      ;
+      });
+  }
+
   public writeValue(newValue: Author[]): void {
     // do not call onChange -> prestine
     if (newValue) {
-      // this.currentValue = [...newValue];
-      // for right checked-state
-      // this.currentValue = this.authorsList
-      //   .filter((author) => newValue
-      //     .some((item) => author.id === item.id)
-      //   )
-      // ;
+      if (!this.fullAuthorsList.length) {
+        this.currentValue = [...newValue];
+      } else {
+        // for right checked-state
+        this.currentValue = this.fullAuthorsList
+          .filter((author) => newValue
+            .some((item) => author.id === item.id)
+          )
+        ;
+      }
       this.changeDetectorRef.markForCheck();
     }
   }
@@ -73,7 +95,7 @@ export class AuthorsComponent implements ControlValueAccessor {
 
   // all other methods work with this
   public set value(newValue: Author[]) {
-    this.currentValue = this.authorsList
+    this.currentValue = this.fullAuthorsList
       .filter((author) => newValue
         .some((item) => author.id === item.id)
       )
--- angular-starter/src/app/course-page/authors/authors.validator.ts
+++ angular-starter/src/app/course-page/authors/authors.validator.ts
@@ -1,6 +1,5 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function validateAuthors(control: AbstractControl): ValidationErrors | null {
-  console.warn(`author.value:${JSON.stringify(control.value)}`);
   return control.value && control.value.length ? null : {authors: true};
 }
