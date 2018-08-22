61f8001ecd0dd03b4e8d17b4751c055eb78458ff
ArlinskiEV <ArlinskiEV@gmail.com>
2018-08-22 14:18:44 +0300

    AMP/editor/set-get_html

--- angular-starter/src/app/course-page/editor-component/editor.component.ts
+++ angular-starter/src/app/course-page/editor-component/editor.component.ts
@@ -87,6 +87,46 @@ export class EditorComponent implements OnInit, OnDestroy {
     console.log(this.quillGetHTML());
   }
 
+  public setHtml() {
+    const container = document.getElementById('editor');
+    container.getElementsByClassName('ql-editor')[0].innerHTML = `<p
+    >      .<span style="color: rgb(220, 220, 170);"
+    >map</span
+    >((<span style="color: rgb(156, 220, 254);"
+    >data</span>: {<span style="color: rgb(156, 220, 254);">new</span
+    >: <span style="color: rgb(78, 201, 176);">boolean</span
+    >, <span style="color: rgb(156, 220, 254);">id</span>: <span style="color: rgb(78, 201, 176);"
+    >any</span>}) <span style="color: rgb(86, 156, 214);">=&gt;</span
+    > {</p><p>        <span style="color: rgb(86, 156, 214);">this</span
+    >.<span style="color: rgb(156, 220, 254);">idInfo</span
+    > = <span style="color: rgb(156, 220, 254);">data</span>;</p><p
+    >        <span style="color: rgb(197, 134, 192);">if</span
+    > (!<span style="color: rgb(156, 220, 254);">data</span
+    >.<span style="color: rgb(156, 220, 254);">new</span
+    >) {</p><p>          <span style="color: rgb(197, 134, 192);"
+    >return</span> <span style="color: rgb(86, 156, 214);">this</span
+    >.<span style="color: rgb(156, 220, 254);">courseService</span
+    >.<span style="color: rgb(220, 220, 170);">getItemById</span
+    >(<span style="color: rgb(156, 220, 254);">data</span
+    >.<span style="color: rgb(156, 220, 254);">id</span>);</p><p
+    >        } <span style="color: rgb(197, 134, 192);">else</span
+    > {</p><p>          <span style="color: rgb(86, 156, 214);"
+    >const</span> <span style="color: rgb(156, 220, 254);">course</span
+    > = <span style="color: rgb(86, 156, 214);">new</span
+    > <span style="color: rgb(78, 201, 176);">Course</span>();</p><p
+    >          <span style="color: rgb(86, 156, 214);">this</span
+    >.<span style="color: rgb(156, 220, 254);">idInfo</span
+    >.<span style="color: rgb(156, 220, 254);">id</span
+    > = <span style="color: rgb(156, 220, 254);">course</span
+    >.<span style="color: rgb(156, 220, 254);">id</span>;</p><p
+    >          <span style="color: rgb(197, 134, 192);">return</span
+    > <span style="color: rgb(156, 220, 254);">Observable</span
+    >.<span style="color: rgb(220, 220, 170);">of</span
+    >(<span style="color: rgb(156, 220, 254);">course</span>);</p><p
+    >        }</p><p>      }).<span style="color: rgb(220, 220, 170);"
+    >switch</span>()</p><p>    ;</p>`;
+  }
+
   public ngOnDestroy() {
     this.log();
   }
